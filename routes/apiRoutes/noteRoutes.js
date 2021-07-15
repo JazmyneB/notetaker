a3e1f6e8f829a4c6cbfe5d2dde80844142d412cd
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-15 15:18:38 -0700

    fixed api issues

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,3 +1,49 @@
 const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+const { filterByQuery, findById, createNewNote } = require('../../public/assets/js/notes');
 const { notes } = require('../../db/db.json');
 
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (res.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
+
+// router.get('/api/notes', (req, res) => {
+    
+//     res.json(notes);
+// })
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result){
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+})
+
+router.post('/notes', (req, res) => {
+    //set id to the note 
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    notes.splice(req.params.id, 1);
+    // fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(notes), (err) => {
+    //     if (err){
+    //         return console.log(err)
+    //     }
+    //     console.log("Your note was deleted");
+    // });
+    res.json(notes);
+    //console.log(notes);
+})
+
+module.exports = router;
