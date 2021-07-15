3f731f04ab5342cc17aa79dc3c9d4f9684106975
JazmyneB <bradleyj1@email.arizona.edu>
2021-07-15 15:19:44 -0700

    fixed api issues

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 //Port
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes')
 //Reads the Data file
 const { notes } = require('./db/db.json');
 
-//ROutes to index page
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
+
+// function createNewNote(body, notesArray){
+//     //console.log(body)
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(path.join(__dirname, '/db/db.json'),
+//     JSON.stringify({ notes: notesArray }, null, 2)
+//     );
+
+//     return note;
+// }
+
+// app.post('/api/notes', (req, res) => {
+//     //set id to the note 
+//     req.body.id = notes.length.toString();
+//     const note = createNewNote(req.body, notes);
+
+//     res.json(note);
+// });
 
 
 
